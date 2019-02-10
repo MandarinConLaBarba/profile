bdedc25101840ba9f9aeb77c9a4f5888fe630fc3
mndrn_ <mandarind@gmail.com>
2019-02-10 14:15:40 -0600

    Updates

--- precache-manifest.bea41004a975b4d6bac9861b480ea384.js
+++ precache-manifest.bea41004a975b4d6bac9861b480ea384.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/profile/static/js/runtime~main.92700b96.js"
   },
   {
-    "revision": "e812244164735be8b7c2",
-    "url": "/profile/static/js/main.e8122441.chunk.js"
+    "revision": "4d7d62c4ac6c9f6e3724",
+    "url": "/profile/static/js/main.4d7d62c4.chunk.js"
   },
   {
     "revision": "6d145eb3c9be2b3f56f6",
     "url": "/profile/static/js/1.6d145eb3.chunk.js"
   },
   {
-    "revision": "e812244164735be8b7c2",
+    "revision": "4d7d62c4ac6c9f6e3724",
     "url": "/profile/static/css/main.9a122c09.chunk.css"
   },
   {
-    "revision": "8fec7da9cbe36500732b9c3f252f71f5",
+    "revision": "153de07e9c6fefeaf8888fc1063ac50b",
     "url": "/profile/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.49eef29bd3064849f76a32a9d8549375.js"
+  "/profile/precache-manifest.bea41004a975b4d6bac9861b480ea384.js"
 );
 
 workbox.clientsClaim();
